b3607e93007d17d8ddd33a399d2fd4f2164415eb
Christopher Warth <christopher.warth@web.de>
2022-12-26 21:49:27 +0100

    refactor: code review changes

--- hooks/useDragAndDrop.js
+++ hooks/useDragAndDrop.js
@@ -7,12 +7,24 @@ const hook = (element, updateElement, index) => {
 		const handleMouseMove = (e) => {
 			let newX = e.clientX - initialX;
 			let newY = e.clientY - initialY;
-			if (newX < 0) newX = 0;
-			if (newX >= 0 && newX <= slideWidth - element.width) newX = newX;
-			if (newX > slideWidth) newX = slideWidth - element.width;
-			if (newY < 0) newY = 0;
-			if (newY >= 0 && newY <= slideHeight - element.height) newY = newY;
-			if (newY > slideHeight) newY = slideHeight - element.height;
+
+			// Check if the right edge of the element is outside of the slide
+			if (newX + element.width > slideWidth) {
+				newX = slideWidth - element.width;
+			}
+			// Check if the bottom edge of the element is outside of the slide
+			if (newY + element.height > slideHeight) {
+				newY = slideHeight - element.height;
+			}
+			// Check if the left edge of the element is outside of the slide
+			if (newX < 0) {
+				newX = 0;
+			}
+			// Check if the top edge of the element is outside of the slide
+			if (newY < 0) {
+				newY = 0;
+			}
+
 			updateElement(index, { ...element, x: newX, y: newY });
 		};
 		handleMouseMoveRef.current = handleMouseMove;
