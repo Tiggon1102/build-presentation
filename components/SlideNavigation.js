f6bf6309a71b9240b9e3f0f652d3b5aef0fe99bd
Christopher Warth <christopher.warth@web.de>
2022-12-28 15:15:53 +0100

    feat: finish styling of slide navigation

--- components/SlideNavigation.js
+++ components/SlideNavigation.js
@@ -10,14 +10,22 @@ const Component = ({ texts, prevSlide, nextSlide, currentSlideIndex, setCurrentS
 		<div style={style.navigationBar}>
 			<div style={style.slideNumbers}>
 				{Array.from(Array(presentationLength).keys()).map((slideNumber) => (
-					<span key={slideNumber} style={style.slideNumber} onClick={() => setCurrentSlideIndex(slideNumber)}>
+					<span
+						key={slideNumber}
+						style={slideNumber === currentSlideIndex ? style.slideNumberActive : style.slideNumber}
+						onClick={() => setCurrentSlideIndex(slideNumber)}
+					>
 						{slideNumber}
 					</span>
 				))}
 			</div>
 			<div style={style.navigationButtons}>
-				<button onClick={prevSlide}>{texts['previous-button']}</button>
-				<button onClick={nextSlide}>{texts['next-button']}</button>
+				<button style={style.navigationButton} onClick={prevSlide}>
+					{texts['previous-button']}
+				</button>
+				<button style={style.navigationButton} onClick={nextSlide}>
+					{texts['next-button']}
+				</button>
 			</div>
 		</div>
 	);
--- components/SlideNavigation.style.js
+++ components/SlideNavigation.style.js
@@ -4,17 +4,42 @@ const getMyStyle = () => ({
 	navigationBar: {
 		display: 'flex',
 		width: '100%',
+		justifyContent: 'space-between',
+		marginTop: '20px',
 	},
 	slideNumbers: {
 		display: 'flex',
+		marginTop: '4px',
 	},
 	slideNumber: {
 		cursor: 'pointer',
 		textDecoration: 'none',
+		marginRight: '5px',
+		fontFamily: template.fonts.main,
+		fontWeight: 'bold',
+	},
+	slideNumberActive: {
+		cursor: 'pointer',
+		textDecoration: 'underline',
+		marginRight: '5px',
+		fontFamily: template.fonts.main,
+		fontWeight: 'bold',
 	},
 	navigationButtons: {
 		display: 'flex',
 		justifyContent: 'flex-end',
+		gap: '3px',
+	},
+	navigationButton: {
+		backgroundColor: template.colors.successButtonBackground,
+		border: 'none',
+		cursor: 'pointer',
+		fontFamily: template.fonts.main,
+		color: template.colors.successButtonLabel,
+		width: '120px',
+		height: '30px',
+		textAlign: 'center',
+		fontWeight: 'bold',
 	},
 });
 
