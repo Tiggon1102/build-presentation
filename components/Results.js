0ef23e362ff099284f8361898717d6cb64e7f714
Christopher Warth <christopher.warth@web.de>
2022-12-29 16:08:03 +0100

    refactor: put texts into localization files

--- components/Results.js
+++ components/Results.js
@@ -7,16 +7,16 @@ const Component = ({ texts, setCurrentStep }) => {
 	return (
 		<div style={style.resultsContainer}>
 			<div style={style.backButton} onClick={() => setCurrentStep('edit')}>
-				Zurück zum Editor
+				{texts['back-to-edit-button']}
 			</div>
-			<h2 style={style.headline}>Results</h2>
+			<h2 style={style.headline}>{texts['results-title']}</h2>
 			<div style={style.imageContainer}>
 				<img
 					alt="img1"
 					src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKr5wT7rfkjkGvNeqgXjBmarC5ZNoZs-H2uMpML8O7Q4F9W-IlUQibBT6IPqyvX45NOgw&usqp=CAU"
 					style={style.presentationImage}
 				/>
-				<p style={style.presentationLabel}>Presentation</p>
+				<p style={style.presentationLabel}>{texts['presentation-label']}</p>
 			</div>
 			<button style={style.exportButton} onClick={() => null}>
 				{texts['export-button']}
