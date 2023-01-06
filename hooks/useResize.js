ad1d7b87e7d8db2da71bd7d36e071f28f2bc9429
MrDevBeast <peterash30@outlook.com>
2023-01-06 18:44:54 -0500

    #first commit

--- hooks/useResize.js
+++ hooks/useResize.js
@@ -2,12 +2,29 @@ import { useRef } from 'react';
 
 const hook = (element, updateElement, index) => {
 	const elementRef = useRef(null);
-
-	const resize = () => {
+	const conX = element.x;
+	const slideWidth = document.getElementById('slide').clientWidth;
+	const slideHeight = document.getElementById('slide').clientHeight;
+	let prevChange = {};
+	const resize = (e, state, ele, change) => {
+		let x, y;
 		const { width, height } = elementRef.current.getBoundingClientRect();
+		switch (state) {
+			case 'topLeft':
+				x = element.x + prevChange.width ? change.width - prevChange.width : change.width;
+				break;
+			case 'bottomLeft':
+				break;
+			case 'topRight':
+				break;
+			case 'bottomRight':
+				break;
+			default:
+				break;
+		}
+		prevChange = change;
 		updateElement(index, { ...element, width, height });
 	};
-
 	return { elementRef, resize };
 };
 
